bbf455d5e759676d4e929e68475af6d13a0bdcec
Rory McLaughlin <rmclaughlin@nectarom.com>
2019-01-31 11:54:05 -0600

    Add dynamic active class to carousel items.

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,18 +4,20 @@ import './Carousel.css';
 class Carousel extends Component {
     render () {
         const { img_list, carousel_id } = this.props;
-        let carouselImages = img_list ? img_list.map((image, i) => {
-                return <div className="carousel-item active" key={i}>
-                    <img className="d-block w-100" src={image.src} alt="need-to-change"/>
-                </div>
-            }) : [];
+
+        let carouselItems = img_list ? img_list.map((item, i) => {
+            let classes = i === 0 ? "carousel-item active" : "carousel-item";
+            return <div className={classes} key={i}>
+                <img className="d-block w-100" src={item.src} alt="need-to-change"/>
+            </div>
+        }) : [];
 
         return (
             <div>
                <div id={carousel_id} className="carousel slide" data-ride="carousel">
-                 <div className="carousel-inner">
-                     {carouselImages}
-                 </div>
+                <div className="carousel-inner">
+                    {carouselItems}
+                </div>
                </div>
                <a className="carousel-control-prev" href={`#${carousel_id}`} role="button" data-slide="prev">
                  <span className="carousel-control-prev-icon" aria-hidden="true">
